aadc30117614e2ec1ca4223f3845f41f1de19a0e
Pyry Jahkola <pyry.jahkola@iki.fi>
2015-01-26 16:48:28 +0200

    Initial commit

---
+++ LevelDB/LevelDB.h
@@ -0,0 +1,19 @@
+//
+//  LevelDB.h
+//  LevelDB
+//
+//  Created by Pyry Jahkola on 26.01.2015.
+//  Copyright (c) 2015 Pyrtsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LevelDB.
+FOUNDATION_EXPORT double LevelDBVersionNumber;
+
+//! Project version string for LevelDB.
+FOUNDATION_EXPORT const unsigned char LevelDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LevelDB/PublicHeader.h>
+
+
